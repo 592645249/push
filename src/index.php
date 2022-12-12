b9100f201389e5ef20be0ce229caa46aaa56c811
zjs <zjs@qq.com>
2022-12-12 13:45:28 +0800

    no message

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php 
+    namespace qcloude\test;
+
+    class Index{
+        public static function test(){
+            echo <<<EOF
+            test,test,test.
+            test,test,test.
+        EOF;
+        }
+    }
